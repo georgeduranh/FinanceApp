8e8bd5c161b531ec986bc34b623f5cad7d162c8f
georgeduranh <duran.jorgeivan@gmail.com>
2022-06-11 17:54:09 -0500

    hello world

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import os
+
 
 #from app
 from app import create_app
 
+appid = os.environ.get('ACCESS_KEY')
 
-#DB
 app = create_app()
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
