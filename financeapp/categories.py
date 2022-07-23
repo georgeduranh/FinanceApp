690ea6b255dfc0b1bfdf0f749cf584c984f502cb
georgeduranh <duran.jorgeivan@gmail.com>
2022-07-22 19:00:38 -0500

    Delete and edit categories

--- financeapp/categories.py
+++ financeapp/categories.py
@@ -11,7 +11,7 @@ from financeapp.db import get_db
 bp = Blueprint('categories', __name__)
 
 
-@bp.route('/categories')
+@bp.route('/categories', )
 @login_required
 def index_cat():
     db = get_db()
@@ -47,4 +47,55 @@ def create():
             return redirect(url_for('categories.index_cat'))
 
     return render_template('categories/create.html')
+
+def get_cat(id, check_author=True):
+    category = get_db().execute(
+        'SELECT *'
+        ' FROM categories c JOIN users u ON c.user_id = u.id'
+        ' WHERE c.id_categories = ?',
+        (id,)
+    ).fetchone()
+
+    if category is None:
+        abort(404, f"Categorty id {id} doesn't exist.")
+
+    if category['user_id'] != g.user['id']:
+        abort(403)
+
+    return category
+
+@bp.route('/categories/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    category = get_cat(id)
+    error = None
+
+    if request.method == 'POST':
+        category = request.form['category']      
+
+        if not category:
+            error = 'Category is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE categories SET category = ?'
+                ' WHERE id_categories = ?',
+                ( category, id)
+            )
+            db.commit()
+            return redirect(url_for('categories.index_cat'))
+
+    return render_template('categories/update.html', category=category)
+
+@bp.route('/categories/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_cat(id)
+    db = get_db()
+    db.execute('DELETE FROM categories WHERE id_categories = ?', (id,))
+    db.commit()
+    return redirect(url_for('categories.index_cat'))
 
